6564e7111d896d0f69518c157a58a28d41c4862e
ymyyh <865895777@qq.com>
2022-11-16 22:16:00 +0800

    version-0.0.1

---
+++ user/src/test/java/com/bitter/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitter.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
